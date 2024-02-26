5ca41b5a36f50cb967e9ed81cc1fcef858e9b900
方先生 <282697706+falenai@users.noreply.github.com>
2024-02-26 08:44:12 +0800

    style: fix trailing whitespace in utils

--- src/utils.py
+++ src/utils.py
@@ -93,3 +93,4 @@ def chunk_list(lst, chunk_size: int):
     """Split list into chunks of at most chunk_size."""
     for i in range(0, len(lst), chunk_size):
         yield lst[i : i + chunk_size]
+
